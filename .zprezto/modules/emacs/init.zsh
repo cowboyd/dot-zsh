5793655596401a8509c38ad6965b1a8a96d8810c
Charles Lowell <cowboyd@frontside.io>
2014-10-05 01:07:09 -0500

    enable emacs server from command line usage

--- .zprezto/modules/emacs/init.zsh
+++ .zprezto/modules/emacs/init.zsh
@@ -9,6 +9,10 @@ if [[ ! -d "$HOME/.cask" ]]; then
   return 1
 fi
 
+
+export EDITOR='emacsclient -t --alternate-editor= $@'
+export VISUAL=$EDITOR
+
 # Prepend Cask bin directory.
 path=($HOME/.cask/bin $path)
 
@@ -19,7 +23,12 @@ source "$HOME/.cask/etc/cask_completion.zsh" 2> /dev/null
 # Aliases
 #
 
+#cask
 alias cai='cask install'
 alias cau='cask update'
 alias caI='cask init'
 alias cae='cask exec'
+
+#server
+alias em='emacsclient -nc $@'
+alias ek='emacsclient -e "(kill-emacs 1)"'
