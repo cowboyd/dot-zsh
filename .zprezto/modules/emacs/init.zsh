864066afd8b8677f97d9bc868467c57894c1606c
Charles Lowell <cowboyd@frontside.io>
2014-10-05 01:43:19 -0500

    was bailing on no cask

--- .zprezto/modules/emacs/init.zsh
+++ .zprezto/modules/emacs/init.zsh
@@ -3,15 +3,24 @@
 #
 # Authors: Sebastian Wiesner <lunaryorn@gmail.com>
 #
+export EDITOR="emacsclient -t --alternate-editor="
+export VISUAL=$EDITOR
+
+#
+# Aliases
+#
+
+#server
+alias em='emacsclient -nc'
+alias es='emacs -nw --daemon'
+alias ek='emacsclient -e "(kill-emacs 0)"'
+alias er='ek;es'
+
 
 # Return if requirements are not found.
 if [[ ! -d "$HOME/.cask" ]]; then
   return 1
 fi
-
-
-export EDITOR="emacsclient -t --alternate-editor="
-export VISUAL=$EDITOR
 
 # Prepend Cask bin directory.
 path=($HOME/.cask/bin $path)
@@ -19,18 +28,9 @@ path=($HOME/.cask/bin $path)
 # Load Carton completion
 source "$HOME/.cask/etc/cask_completion.zsh" 2> /dev/null
 
-#
-# Aliases
-#
 
 #cask
 alias cai='cask install'
 alias cau='cask update'
 alias caI='cask init'
 alias cae='cask exec'
-
-#server
-alias em='emacsclient -nc'
-alias es='emacs -nw --daemon'
-alias ek='emacsclient -e "(kill-emacs 0)"'
-alias er='ek;es'
